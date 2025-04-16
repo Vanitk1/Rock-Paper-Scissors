6ac2401f14eeb97a60538f09a6c575fda89960bf
Vanit Kashab <vanitk1@gmail.com>
2025-04-16 22:15:51 +0100

    refactored js

---
+++ Rock-Paper-Scissors.test.js
@@ -0,0 +1,20 @@
+const {playRound, computerPlay, capitalize} = require("./Rock-Paper-Scissors-V2");
+
+describe("capitalize", () => {
+    test("capitalises first letter of a word", () => {
+    expect(capitalize('rock')).toBe('Rock');
+    expect(capitalize('paper')).toBe('Paper');
+    expect(capitalize('scissors')).toBe('Scissors');
+    });
+});
+
+describe('computerPlay()', () => {
+    test('returns a valid choice', () => {
+
+      const result = computerPlay();
+
+      const choices = ['rock', 'paper', 'scissors'];
+
+      expect(choices).toContain(result);
+    });
+  });
